03b019d798ce958f2e311440ed57427972ff95ce
Leon-oss201 <zhuyanglong_1122@163.com>
2026-08-13 19:50:52 +0800

    v24: anonymous readApiRaw retry when token is 401 + forceRescue reload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker for Lattiao Travel Journal PWA v23
-const CACHE_NAME = 'lattiao-travel-v23';
+// Service Worker for Lattiao Travel Journal PWA v24
+const CACHE_NAME = 'lattiao-travel-v24';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
