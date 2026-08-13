87fb67bf1ecc3ad1cf34e3a2a1932c9360ead494
Leon-oss201 <zhuyanglong_1122@163.com>
2026-08-13 20:10:56 +0800

    v27: openTrip auto-pick first day with locations

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker for Lattiao Travel Journal PWA v26
-const CACHE_NAME = 'lattiao-travel-v26';
+// Service Worker for Lattiao Travel Journal PWA v27
+const CACHE_NAME = 'lattiao-travel-v27';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
