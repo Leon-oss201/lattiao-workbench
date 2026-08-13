1b9b459bd1f653b4e0f4feeb6e7220da57801b52
Leon-oss201 <zhuyanglong_1122@163.com>
2026-08-14 01:03:50 +0800

    v40: forceResync now does hard-reset (clear local + reload) to guarantee cloud fetch

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker for Lattiao Travel Journal PWA v39
-const CACHE_NAME = 'lattiao-travel-v39';
+// Service Worker for Lattiao Travel Journal PWA v40
+const CACHE_NAME = 'lattiao-travel-v40';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
