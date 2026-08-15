059daad983e5bef0473e503c86dd32ec195cc23a
Leon-oss201 <zhuyanglong_1122@163.com>
2026-08-15 18:52:02 +0800

    v49: add '📷 仅拉取照片' button (forceRemote=true, doesn't wipe local)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker for Lattiao Travel Journal PWA v48
-const CACHE_NAME = 'lattiao-travel-v48';
+// Service Worker for Lattiao Travel Journal PWA v49
+const CACHE_NAME = 'lattiao-travel-v49';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
