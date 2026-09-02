c4f3123dd17f5604ba1a7fad9c09a11aa89e5d73
Leon-oss201 <zhuyanglong_1122@163.com>
2026-09-02 21:40:13 +0800

    bump sw cache to v119

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Lattiao Travel Journal PWA v98
-const CACHE_NAME = 'lattiao-travel-v118';
+const CACHE_NAME = 'lattiao-travel-v119';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
