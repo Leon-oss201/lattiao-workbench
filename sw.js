e33cb2c0d5b3aa249527c6aacb91377aeee7adc1
Leon-oss201 <zhuyanglong_1122@163.com>
2026-09-04 02:03:26 +0800

    deploy v134: 照片通知(加照片给协作者发通知) + 清空通知持久化

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Lattiao Travel Journal PWA v98
-const CACHE_NAME = 'lattiao-travel-v133';
+const CACHE_NAME = 'lattiao-travel-v134';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
